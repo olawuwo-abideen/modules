f755badc5561c679c6f3ceea3ead198258dee946
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-13 14:19:48 +0100

    add route

--- hapi.js
+++ hapi.js
@@ -1,10 +1,25 @@
-// const hapi = require('@hapi/hapi'); 
-// //  var server = new hapi.Server() 
-//  const server = hapi.server({ load: { sampleInterval: 1000 } });
-//  server.connection({ port: 3000 }) 
-//  server.start(function() { 
-//  console.log("Server running at: ", server.info.url) 
-//  }) 
+// 'use strict';
+
+// const Hapi = require('@hapi/hapi');
+
+// const init = async () => {
+
+//     const server = Hapi.server({
+//         port: 3000,
+//         host: 'localhost'
+//     });
+
+//     await server.start();
+//     console.log('Server running on %s', server.info.uri);
+// };
+
+// process.on('unhandledRejection', (err) => {
+
+//     console.log(err);
+//     process.exit(1);
+// });
+
+// init();
 
 'use strict';
 
@@ -15,6 +30,15 @@ const init = async () => {
     const server = Hapi.server({
         port: 3000,
         host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
     });
 
     await server.start();
@@ -28,4 +52,3 @@ process.on('unhandledRejection', (err) => {
 });
 
 init();
-

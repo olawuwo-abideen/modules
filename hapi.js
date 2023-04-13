02f09541775584032b9c1a98cd58b4e6e16ecb77
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-13 14:28:36 +0100

    get request

--- hapi.js
+++ hapi.js
@@ -1,26 +1,3 @@
-// 'use strict';
-
-// const Hapi = require('@hapi/hapi');
-
-// const init = async () => {
-
-//     const server = Hapi.server({
-//         port: 3000,
-//         host: 'localhost'
-//     });
-
-//     await server.start();
-//     console.log('Server running on %s', server.info.uri);
-// };
-
-// process.on('unhandledRejection', (err) => {
-
-//     console.log(err);
-//     process.exit(1);
-// });
-
-// init();
-
 'use strict';
 
 const Hapi = require('@hapi/hapi');
@@ -38,6 +15,14 @@ const init = async () => {
         handler: (request, h) => {
 
             return 'Hello World!';
+        }
+    });
+    server.route({
+        method: 'GET',
+        path: '/books',
+        handler: (request, h) => {
+
+            return 'There is no books available';
         }
     });
 

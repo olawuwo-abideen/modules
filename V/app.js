a92a075b45e6033da8d4d832128b325086d5cd4f
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-14 02:00:16 +0100

    start server

--- V/app.js
+++
@@ -1,8 +0,0 @@
-var Vatican = require("vatican") 
- //Use all default settings 
- var app = new Vatican() 
- app.dbStart(function() { 
- console.log("Db connection stablished...") 
- //Start the server 
- app.start() 
- } )

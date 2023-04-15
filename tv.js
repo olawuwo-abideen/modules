861bce33a5da57ad070ed7f812932a6f8d6bbec5
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-15 02:40:33 +0100

    tv4.

---
+++ tv.js
@@ -0,0 +1,26 @@
+var validator = require("tv4") 
+ var schema ={ 
+ "title": "Example Schema", 
+ "type": "object", 
+ "properties": { 
+ "firstName": { 
+ "type": "string" 
+ }, 
+ "lastName": { 
+ "type": "string" 
+ }, 
+ "age": { 
+ "description": "Age in years", 
+ "type": "integer", 
+ "minimum": 0 
+ } 
+ }, 
+ "required": ["firstName", "lastName"] 
+ } 
+ var invalidInput = { 
+ firstName: 'olawuwo', 
+ lastName: 'abideen',
+ age: "100" 
+ } 
+ var results = validator.validateMultiple(invalidInput, schema) 
+ console.log(results) 
